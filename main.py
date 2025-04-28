2eb898f0c2752b4a0a795dd39c71b1ecbfae8590
jacksterdealeo <oswaldtechnic@zoho.com>
2025-04-28 02:14:14 -0800

    command line argument now defines where to read a book from

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import word_count
 from stats import char_count
 from stats import sort_char_dict
@@ -9,7 +11,11 @@ def get_book_text(filepath):
     return text
 
 def main():
-    book_location = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_location = sys.argv[1]
     text = get_book_text(book_location)
     print(word_count(text), "words found in the document")
     count = char_count(text)
